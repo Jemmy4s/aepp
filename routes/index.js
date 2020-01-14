66f0f4ffc55b078f821156fc8ce5091fcb57d18c
vvaleze <vvaleze@datasolution.fr>
2020-01-14 17:46:22 +0100

    add  : header

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const key_fr = {
+  key : {
+    residence : 'Résidence',
+    room : 'Logement',
+    bigEvent : 'Evénement' ,
+    location : 'location',
+    gallery: 'Galerie'
+  }
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', key_fr });
 });
 
 module.exports = router;
