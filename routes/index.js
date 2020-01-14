643d12ed5298a24b7e82deae1e6bf7adc2bec219
vvaleze <vvaleze@datasolution.fr>
2020-01-14 18:38:29 +0100

    add : machin truc

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,21 @@ const key_fr = {
   }
 }
 
+const blocPresentation = {
+    title : 'AEPP',
+    text : " L’Association des Etudiants Protestants de Paris dite A.E.P.P. est une association loi de 1901 reconnue d’utilité publique en 1926. \
+        L’A.E.P.P. se veut un espace de réflexion et de partage ayant le souci d’aider les étudiants à faire de leur période de formation universitaire un véritable temps de développement humain. \
+        La résidence étudiante est le cœur de l’association, avec l’apprentissage exceptionnel que représente la vie communautaire. Elle reçoit des étudiants garçons et filles de tous  horizons géographiques culturels et religieux avec le souci de leur manifester un accueil chaleureux, stimulant et ouvert. ",
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', key_fr });
+  res.render('index', { title: 'Express',   key : {
+      residence : 'Résidence',
+      room : 'Logement',
+      bigEvent : 'Evénement' ,
+      location : 'location',
+      gallery: 'Galerie',
+      presentation :  blocPresentation}});
 });
 
 module.exports = router;
